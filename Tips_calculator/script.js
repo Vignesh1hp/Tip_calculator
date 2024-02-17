16a5d356af746b9e4207b26aade9ada3d2f0645d
Vignesh1hp <vigneshkumarb.sc2004@gmail.com>
2024-02-17 10:48:18 +0530

    feat:finished the html and javascript code.

--- Tips_calculator/script.js
+++ Tips_calculator/script.js
@@ -4,11 +4,13 @@ let calculate = document.getElementById('calculate_btn');
 // adding event listener for clicking the calculate button.
 calculate.addEventListener('click',function(){
     // selecting and storing value of bill amount input.
-    let bill_Amount = document.getElementById('billAmount').value;
+    let bill_Amount = parseFloat(document.getElementById('billAmount').value);
     // selecting and storing value of tip percentage input.
-    let tip_Percentage = document.getElementById('tipsPecentage').value;
+    let tip_Percentage = parseFloat(document.getElementById('tipsPercentage').value);
     // calculating tip amount.
-    let tip_Amount = document.getElementById('tipAmount').value = (bill_Amount/tip_Percentage).toFixed(2);
+    let tip_Amount = (bill_Amount * (tip_Percentage/100)).toFixed(2);
+    document.getElementById('tipAmount').value = tip_Amount;
     // calculating total amount.
-    document.getElementById('totalAmount').value = (parseFloat(bill_Amount)+parseFloat(tip_Amount)).toFixed(2);
+    let total_Amount = (bill_Amount+parseFloat(tip_Amount)).toFixed(2);
+    document.getElementById('totalAmount').value = total_Amount;
 })
